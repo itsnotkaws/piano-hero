9714d9a3e131012b57d55ad059134d5eed0b9453
4shktz <evilancasteras@edenschool.fr>
2024-04-10 11:30:29 +0200

    added midi methods

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-const piano = document.getElementsByClassName("piano");
+function midiMessageReceived(event) {
+    console.log('MIDI message received:', event);
+}
+
+function onMIDISuccess(midiAccess) {
+    console.log('MIDI Access Granted');
+    initDevices(midiAccess);
+}
+
+function onMIDIFailure() {
+    console.error('MIDI Access Denied');
+}
+
+navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 
 window.onload = () => {
+    const piano = document.getElementsByClassName("piano");
     for (let i = 1; i <= 24; i++) {
         const div = document.createElement("div");
         div.classList.add(i <= 10 ? "black" : "white");
         piano[0].appendChild(div);
+        div.addEventListener('click', () => {
+            console.log("test");
+        });
     }
 }
