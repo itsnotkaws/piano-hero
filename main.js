604708548bfbc337f1bc04018230190728fb8106
4shktz <evilancasteras@edenschool.fr>
2024-04-10 10:06:00 +0200

    removed useless class

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const piano = document.getElementsByClassName("piano");
 window.onload = () => {
     for (let i = 1; i <= 24; i++) {
         const div = document.createElement("div");
-        div.classList.add("key", i <= 10 ? "black" : "white");
+        div.classList.add(i <= 10 ? "black" : "white");
         piano[0].appendChild(div);
     }
 }
