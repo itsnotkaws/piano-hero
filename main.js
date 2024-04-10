24bab292c435da130cdf93d07b83cd4be004e21b
4shktz <evilancasteras@edenschool.fr>
2024-04-10 09:50:23 +0200

    added the logic for the keys

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const piano = document.getElementsByClassName("piano");
 
 window.onload = () => {
     for (let i = 1; i <= 24; i++) {
-        
+        const div = document.createElement("div");
+        div.classList.add("key", i <= 10 ? "black" : "white");
+        piano[0].appendChild(div);
     }
 }
