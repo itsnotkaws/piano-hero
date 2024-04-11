a482be66502c3c52fe73111a64ed3db27b968db4
4shktz <evilancasteras@edenschool.fr>
2024-04-11 14:13:25 +0200

    added new receive message

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 function midiMessageReceived(event) {
-    console.log('MIDI message received:', event);
+    const NOTE_ON = 9;
+    const NOTE_OFF = 8;
+
+    const cmd = event.data[0] >> 4;
+    const pitch = event.data[1];
+    const timestamp = Date.now();
+
+    if (cmd === NOTE_OFF || (cmd === NOTE_ON === 0)) {
+        console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}`);
+
+        const note = notesOn.get(pitch);
+        if (note) {
+            console.log(`🎵 pitch:${pitch}, duration:${timestamp - note} ms.`);
+            notesOn.delete(pitch);
+        }
+    } else if (cmd === NOTE_ON) {
+        console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}`);
+
+        notesOn.set(pitch, timestamp);
+    }
 }
+
+const notesOn = new Map();
 
 function onMIDISuccess(midiAccess) {
     console.log('MIDI Access Granted');
