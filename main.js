f19acaf9b93cc164b852be5d63dd101e5c2a90f1
4shktz <evilancasteras@edenschool.fr>
2024-04-11 14:01:04 +0200

    init devices

--- main.js
+++ main.js
@@ -13,6 +13,29 @@ function onMIDIFailure() {
 
 navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 
+function initDevices(midiAccess) {
+    midiIn = [];
+    midiOut = [];
+
+    const inputs = midiAccess.inputs.values();
+    for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+        midiIn.push(input.value);
+    }
+
+    const outputs = midiAccess.outputs.values();
+    for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
+        midiOut.push(output.value);
+    }
+
+    startListening();
+}
+
+function startListening() {
+    for (const input of midiIn) {
+        input.addEventListener('midimessage', midiMessageReceived);
+    }
+}
+
 window.onload = () => {
     const piano = document.getElementsByClassName("piano");
     for (let i = 1; i <= 24; i++) {
